1c5caa1700b71507cfe1d5391496c91392f07a0b
Rohan-developer7 <rohanzore45@gmail.com>
2025-09-13 11:21:45 +0530

    none

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("✅ MongoDB connected"))
+  .then(() => console.log("✅ MongoDB connected "))
   .catch((err) => console.error("❌ MongoDB error:", err));
 
 const app = express();
